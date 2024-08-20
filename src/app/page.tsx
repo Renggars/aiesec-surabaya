9f58ae586c3be079f0da0c53c40edf1b725755cc
Rengga <renggarendy22@gmail.com>
2024-08-20 20:06:59 +0700

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,46 +4,103 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <header className="w-full flex justify-between items-center fixe p-5 border border-b-1 border-gray-300 h-20">
-        <div className="ml-5">
+      <header className="w-full flex justify-between items-center fixed p-5 border border-b-1 border-gray-300 h-28 z-50 bg-white top-0">
+        <Link href={"/"} className="ml-5 cursor-pointer">
           <Image
-            src="/aiesecLogo.svg"
+            src="/aiesecblue.webp"
             alt="Vercel Logo"
-            width={195}
-            height={40}
+            width={250}
+            height={190}
             priority
           />
-        </div>
-        <div className="flex gap-5 mr-10 cursor-pointer">
-          <div>Our partners</div>
-          <div>Our Events</div>
-          <div>Our Blog</div>
-          <div>More</div>
+        </Link>
+        <div className="flex gap-10 mr-12 cursor-pointer text-[#6d6e70] text-lg font-medium transation ease-in-out duration-100">
+          <Link className="hover:text-primary  " href={"#programs"}>
+            Our Programs
+          </Link>
+          <Link
+            className="hover:text-primary transation ease-in-out duration-100"
+            href={"#events"}
+          >
+            Our Events
+          </Link>
+          <Link
+            className="hover:text-primary transation ease-in-out duration-100"
+            href={"#blog"}
+          >
+            Blog
+          </Link>
+          <Link
+            className="hover:text-primary transation ease-in-out duration-100"
+            href={"#contact"}
+          >
+            Contact
+          </Link>
+          <div className="hover:text-primary transation ease-in-out duration-100">
+            More
+          </div>
         </div>
       </header>
-      {/* about */}
-      <div className="">
-        <div className="text-3xl font-semibold text-center mt-9">
-          About Aiesec Surabaya
+
+      {/* Home */}
+      <div className="flex justify-center mt-28">
+        <div className="relative w-full h-[calc(100vh-7rem)] opacity-30">
+          <Image
+            src={"/blog.webp"}
+            alt="blog"
+            layout="fill"
+            objectFit="cover"
+          ></Image>
         </div>
-        <p className="text-center text-2xl px-96 mt-10">
-          AIESEC is a global youth-led organization striving to achieve peace
-          and fulfillment of humankind’s potential by activating leadership
-          qualities in youth. AIESEC spreads across 106 nations and territories,
-          including Indonesia. AIESEC in Indonesia established in 1984 aims to
-          develop youth to make a better Indonesia right now and in the future.
-        </p>
-        <div className="text-center text-2xl font-semibold mt-10 text-blue-500">
-          Know Us More
+        <div className="absolute font-sans top-1/3  left-1/2 -translate-x-1/2 -translate-y-1/2 w-auto h-auto bg-primary flex justify-center items-center p-5 rounded-sm">
+          <p className="text-white font-bold text-5xl">
+            Welcome to AIESEC Surabaya
+          </p>
         </div>
+        <div className="absolute font-sans top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  justify-center items-center">
+          <p className="text-[#1d2129] font-bold text-6xl text-center mt-5">
+            Leadership is for
+          </p>
+          <p className="text-[#1d2129] font-bold text-6xl mt-5">
+            every young person
+          </p>
+        </div>
+        <div className="absolute font-sans top-2/3 left-1/2 -translate-x-1/2 -translate-y-1/2  justify-center items-center">
+          <p className="text-gray-700 font-bold text-xl text-center mt-3">
+            Take part in one of our programs in the city of Surabaya. Start your
+            leadership journey with AIESEC.
+          </p>
+        </div>
+        <button className="absolute font-sans top-3/4  left-1/2 -translate-x-1/2 -translate-y-1/2 w-auto h-auto bg-primary flex justify-center items-center py-2 px-3  text-white text-2xl font-semibold rounded-md mt-5">
+          Let{"'"}s get started!
+        </button>
       </div>
+      {/* Our Programs */}
+      <div id="programs">
+        <div className="text-5xl font-bold text-center mt-10">Our Programs</div>
+        <p className="text-2xl text-gray-700 mt-5 text-center">
+          We aim to develop leadership qualities and capabilities in
+        </p>
+        <p className="text-2xl text-gray-700 mt-5 text-center">
+          young people with these programs
+        </p>
+      </div>
+
       {/* programs internasional */}
-      <div>
-        <div className="text-3xl font-semibold text-center mt-9">
+      <div className="flex justify-center mt-20">
+        <div className="text-3xl font-semibold text-center mt-9 w-[50%] text-gray-600">
           Our Internasional Programs
         </div>
-        <div className="grid grid-cols-2 gap-5 px-56 mt-10">
-          <div className="bg-blue-500">
+        <div className="grid grid-cols-2 gap-5 px-56 mt-10 w-[50%]">
+          <div className="bg-[#0cb9c1] h-80 w-96 flex justify-center items-center p-10">
+            <Image
+              src={"/globalTalent.png"}
+              alt="global teacher"
+              width={400}
+              height={400}
+            />
+          </div>
+          {/* <div className="bg-[#f48924]">
             <Image
               src={"/globalTeacher.png"}
               alt="global teacher"
@@ -51,81 +108,75 @@ export default function Home() {
               height={400}
             />
           </div>
-          <div className="bg-orange-400">
+          <div className="bg-[#037ef3]">
             <Image
-              src={"/globalTeacher.png"}
+              src={"/aiesecMember.webp"}
               alt="global teacher"
               width={400}
               height={400}
             />
           </div>
-          <div className="bg-blue-500">
+          <div className="bg-[#f85a40]">
             <Image
-              src={"/globalTeacher.png"}
+              src={"/globalVolunteer.png"}
               alt="global teacher"
               width={400}
               height={400}
             />
-          </div>
-          <div className="bg-orange-400">
-            <Image
-              src={"/globalTeacher.png"}
-              alt="global teacher"
-              width={400}
-              height={400}
-            />
-          </div>
+          </div> */}
         </div>
       </div>
+
       {/* program local */}
-      <div>
-        <div className="text-3xl font-semibold text-center mt-9">
+      <div className="flex justify-center  mt-20">
+        <div className="grid grid-cols-2 gap-5 px-56 mt-10 w-[50%]">
+          <div className="bg-blue-500 h-80 w-96 flex justify-center items-center p-10">
+            <Image
+              src={"/aiesecFuture.webp"}
+              alt="global teacher"
+              width={400}
+              height={400}
+            />
+          </div>
+          {/* <div className="bg-orange-400">
+            <Image
+              src={"/locaProject.png"}
+              alt="global teacher"
+              width={400}
+              height={400}
+            />
+          </div>
+          <div className="bg-blue-500">
+            <Image
+              src={"/localVolunteer.webp"}
+              alt="global teacher"
+              width={400}
+              height={400}
+            />
+          </div> */}
+        </div>
+        <div className="text-3xl font-semibold text-center mt-9 w-[50%] text-gray-600">
           Our Local Programs
         </div>
-        <div className="grid grid-cols-2 gap-5 px-56 mt-10">
-          <div className="bg-blue-500">
-            <Image
-              src={"/globalTeacher.png"}
-              alt="global teacher"
-              width={400}
-              height={400}
-            />
-          </div>
-          <div className="bg-orange-400">
-            <Image
-              src={"/globalTeacher.png"}
-              alt="global teacher"
-              width={400}
-              height={400}
-            />
-          </div>
-          <div className="bg-blue-500">
-            <Image
-              src={"/globalTeacher.png"}
-              alt="global teacher"
-              width={400}
-              height={400}
-            />
-          </div>
-        </div>
       </div>
+
       {/* our event */}
-      <div>
-        <div className="text-3xl font-semibold text-center mt-9">
+      <div id="events">
+        <div className="text-gray-600 text-3xl font-semibold text-center mt-20">
           Our Events
         </div>
-        <div className="grid grid-cols-2 gap-5 px-56 mt-10">
-          <div className="bg-blue-500">
+        <div className="grid grid-cols-2 gap-5 px-56 mt-14">
+          <div className="bg-blue-500 flex justify-center items-center">
             <Image
-              src={"/globalTeacher.png"}
+              src={"/impactCircle.webp"}
               alt="global teacher"
               width={400}
               height={400}
             />
           </div>
-          <div className="bg-orange-400">
+          <div className="bg-orange-400 flex justify-center items-center">
             <Image
-              src={"/globalTeacher.png"}
+              src={"/youthToday.webp"}
               alt="global teacher"
               width={400}
               height={400}
@@ -133,20 +184,22 @@ export default function Home() {
           </div>
         </div>
       </div>
+
       {/* blog */}
-      <div>
+      <div id="blog" className="mt-20">
         <div className="text-3xl font-semibold text-center mt-9">Blog</div>
         <div className="flex justify-center mt-10">
           <Image src={"/blog.webp"} alt="blog" width={1200} height={500} />
         </div>
       </div>
-      {/* partner with us ? */}
-      <div className="flex justify-center gap-10 mt-10 px-44">
+
+      {/* Contact ? */}
+      <div id="contact" className="flex justify-center gap-10 mt-20 px-44">
         <div className="w-1/2">
           <div className="text-3xl font-semibold text-center">
             Partner With Us?
           </div>
-          <div className="h-1 w-48 bg-blue-500 my-5" />
+          <div className="h-1 w-48 bg-primary my-5" />
           <p>
             AIESEC is a global platform for young people to explore and develop
             their leadership potential. We are a non-political, independent,
@@ -154,6 +207,7 @@ export default function Home() {
             institutions of higher education.
           </p>
         </div>
+
         <div className="w-1/2">
           <form
             action="https://getform.io/f/zazkpxob"
@@ -205,16 +259,17 @@ export default function Home() {
                 ></textarea>
               </div>
               <div className="w-full text-center">
-                <button className="bg-blue-500 text-base font-semibold text-white bg-green-main py-1 px-9 rounded-full sm:w-32 lg:w-60 xl:w-72 hover:opacity-80 hover:shadow-lg transition duration-500">
-                  Send
+                <button className="bg-primary text-base font-semibold text-white bg-green-main py-1 px-9 rounded-full sm:w-32 lg:w-60 xl:w-72 hover:opacity-80 hover:shadow-lg transition duration-500">
+                  Submit
                 </button>
               </div>
             </div>
           </form>
         </div>
       </div>
+
       {/* partner list */}
-      <div>
+      <div className="mt-20">
         <div className="text-3xl font-semibold text-center mt-10">
           Out Partners
         </div>
@@ -262,6 +317,7 @@ export default function Home() {
           <Image src={"/shopee.webp"} alt="goto" width={100} height={100} />
         </div>
       </div>
+
       {/* footer */}
       <div className="flex bg-gray-900 p-10 mt-10">
         <div className="grid grid-cols-3 gap-10 px-28">
@@ -295,7 +351,7 @@ export default function Home() {
           </div>
           {/* journey */}
           <div className="ml-24">
-            <div className="text-2xl text-blue-500">Our Journey</div>
+            <div className="text-2xl text-primary">Our Journey</div>
             <div className="ml-5 text-lg  text-white">Categories</div>
             <div className="flex flex-col text-white ml-10">
               <Link href={"#"}>AIESEC Jouerney</Link>
@@ -307,9 +363,9 @@ export default function Home() {
               <Link href={"#"}>Youth</Link>
             </div>
           </div>
-          {/* office map */}
+          {/*sosial Media and office map */}
           <div className="">
-            <div className="text-xl text-blue-500">Sosial Media</div>
+            <div className="text-xl text-primary">Sosial Media</div>
             <div className="flex items-center  mt-5 text-white">
               <Link
                 href="https://www.facebook.com/ngga.rengga.75?mibextid=LQQJ4D"
@@ -380,7 +436,7 @@ export default function Home() {
                 </svg>
               </Link>
             </div>
-            <div className="text-2xl text-blue-500 mt-10">
+            <div className="text-2xl text-primary mt-10">
               AIESEC Surabaya Office
             </div>
             <div className="h-20 w-36 bg-gray-200 mt-4"></div>
